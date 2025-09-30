f33b9b6edd7229b86b0f0180c8e85dc4caa5801a
IvanClarion <ivandaleclarion@gmail.com>
2025-09-30 12:57:01 +0800

    meet the team ui fixed

--- src/components/dev/MeetTheDev.jsx
+++ src/components/dev/MeetTheDev.jsx
@@ -4,9 +4,9 @@ function MeetTheDev() {
   return (
     <section>
       <div className='text-center'>
-        <h1 className='text-4xl'> Meet the Developers</h1>
+        <h1 className='text-4xl'> Meet the Team</h1>
       </div>
-      <div className='flex relative overflow-hidden justify-center items-center gap-10'>
+      <div className='flex flex-col lg:flex-row relative overflow-hidden justify-center items-center gap-10'>
             <div className='devs-card'>
                 <figure>
                     <img src={DevsSample} alt="" />
@@ -16,30 +16,30 @@ function MeetTheDev() {
                     <label>Cross-Platform Developer</label>
                     <p className='text-sm text-gray-200'>Ivan Dale Clarion</p>
                 </article>
-                
             </div>
+        <div className='devs-card'>
+                <figure>
+                    <img src={DevsSample} alt="" />
+                    <span className='devs-span-box'/>
+                </figure>
+                <article >
+                    <label>Project Manager</label>
+                    <p className='text-sm text-gray-200'>Nicholas Rye Laoguico</p>
+                </article>
+            </div>
+
             <div className='devs-card'>
                 <figure>
                     <img src={DevsSample} alt="" />
                     <span className='devs-span-box'/>
                 </figure>
                 <article >
-                    <label>Cross-Platform Developer</label>
-                    <p className='text-sm text-gray-200'>Ivan Dale Clarion</p>
+                    <label>Web Developer</label>
+                    <p className='text-sm text-gray-200'>Kirk Justin Mercado</p>
                 </article>
-                
             </div>
-            <div className='devs-card'>
-                <figure>
-                    <img src={DevsSample} alt="" />
-                    <span className='devs-span-box'/>
-                </figure>
-                <article >
-                    <label>Cross-Platform Developer</label>
-                    <p className='text-sm text-gray-200'>Ivan Dale Clarion</p>
-                </article>
-                
-            </div>
+            
+           
       </div>
     </section>
   )
--- src/components/navBar/MainNavBar.jsx
+++ src/components/navBar/MainNavBar.jsx
@@ -4,7 +4,7 @@ import Logo from '../../assets/images/logo.svg'
 import { MoveRight } from 'lucide-react'
 function mainBar() {
   return (
-    <aside className='m-2 sticky top-2'>
+    <aside className='m-2 sticky z-20 top-2'>
         <nav>
             <ul>
             <figure className='logo'>
